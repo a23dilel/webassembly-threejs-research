a31991d49494d8429b32e3e9267e47147fdcd851
a23dilel <a23dilel@student.his.se>
2026-01-20 14:43:06 +0100

    Add gui controller: performance and GPU Memory

---
+++ src/js/gui.js
@@ -0,0 +1,33 @@
+import GUI from 'lil-gui';
+
+let gui; 
+
+const PARAMS = {
+    // Performance
+    fps: 0,
+    calls: 0,
+    triangles: 0,
+    JsHeapMB: 0,
+    
+    // GPU Memory
+    geometries: 0,
+    textures: 0
+}
+
+function initGUI() {
+    gui = new GUI(document.body);
+
+    // Controller properties
+
+    const PERF_FOLDER = gui.addFolder( 'Performance' );
+    PERF_FOLDER.add( PARAMS, 'fps' ).listen();
+    PERF_FOLDER.add( PARAMS, 'calls' ).listen();
+    PERF_FOLDER.add( PARAMS, 'triangles' ).listen();
+    PERF_FOLDER.add(PARAMS, 'JsHeapMB').listen();
+    
+    const GPU_MEMORY_FOLDER = gui.addFolder( 'GPU Memory' );
+    GPU_MEMORY_FOLDER.add( PARAMS, 'geometries' ).listen();
+    GPU_MEMORY_FOLDER.add( PARAMS, 'textures' ).listen();
+}
+
+export { PARAMS, initGUI }
